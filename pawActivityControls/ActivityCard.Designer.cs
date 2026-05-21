da95c1360327b9152f88ce18159c0ad6168f3f5d
Visan Andrei <andreyvisan1279@gmail.com>
2026-05-21 12:12:03 +0300

    Added activity controls

---
+++ pawActivityControls/ActivityCard.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace pawActivityControls
+{
+    partial class ActivityCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(139, 125);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(44, 16);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "label1";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(139, 176);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(44, 16);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "label2";
+            this.lblDate.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(139, 227);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(44, 16);
+            this.lblCategory.TabIndex = 2;
+            this.lblCategory.Text = "label3";
+            // 
+            // ActivityCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "ActivityCard";
+            this.Size = new System.Drawing.Size(630, 449);
+            this.Load += new System.EventHandler(this.ActivityCard_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblCategory;
+    }
+}
---
+++ pawActivityControls/ActivityCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pawActivityControls
+{
+    public partial class ActivityCard : UserControl
+    {
+        public ActivityCard()
+        {
+            InitializeComponent();
+        }
+
+        private void ActivityCard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void SetData(string title, string date, string category)
+        {
+            lblTitle.Text = title;
+            lblDate.Text = date;
+            lblCategory.Text = category;
+        }
+    }
+}
